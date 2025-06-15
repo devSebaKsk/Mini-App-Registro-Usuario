dcaa63cdab7a05cf57e747bfed05557694602e3d
Sebastian Cascante <139644093+devSebaKsk@users.noreply.github.com>
2025-06-15 12:23:11 +0000

    Creacion de tabla user con endpoint para registro

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,10 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from dotenv import load_dotenv
 from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt, get_jti, verify_jwt_in_request
+from flask_bcrypt import Bcrypt
+from datetime import datetime, timezone, timedelta
+
 
 load_dotenv()
 
@@ -10,15 +14,21 @@ app = Flask(__name__)
 CORS(app)
 
 
+
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+
 db = SQLAlchemy()
-from .models import User
-
-
+from .models import User, BlackListToken
 
 db.init_app(app)
+bcrypt = Bcrypt(app)
+jwt = JWTManager(app)
+
+
+app.config["JWT_SECRET_KEY"] = "nuestra_clave_secreta"
+
 
 with app.app_context():
     db.create_all()
@@ -26,7 +36,42 @@ with app.app_context():
 
 
 
-# Ruta para obtener todos los usuarios
+@jwt.token_in_blocklist_loader
+def handle_revoked_token(jwt_header, jwt_payload):
+    jti = jwt_payload['jti']
+
+    token = db.session.execute(
+        db.select(BlackListToken).filter_by(jti=jti)).scalar()
+    return token is not None
+
+
+
+@app.route("/createUser", methods=["POST"])
+def create_user():
+    data = request.get_json(silent=True)
+    if not data or not data.get("email") or not data.get("password"):
+        return jsonify({"error": "Datos inválidos"}), 400
+
+    if db.session.execute(db.select(User).filter_by(email=data["email"])).scalar_one_or_none():
+        return jsonify({"error": "Usuario ya existe"}), 409
+
+    pw_hash = bcrypt.generate_password_hash(data["password"]).decode('utf8')
+    date = datetime.now(timezone.utc)
+
+    user = User(
+        full_name=data["fullname"],
+        email=data["email"],
+        password=pw_hash,
+        created_at=date
+    )
+
+    db.session.add(user)
+    db.session.commit()
+    return jsonify({"message": "Usuario registrado"}), 201
+
+
+
+
 @app.route('/users', methods=['GET'])
 def get_users():
     users = User.query.all()
--- backend/models.py
+++ backend/models.py
@@ -1,4 +1,7 @@
 from .app import db
+from datetime import datetime, timezone
+from flask_sqlalchemy import SQLAlchemy
+
 
 class User(db.Model):
     __tablename__ = 'users' 
@@ -7,9 +10,19 @@ class User(db.Model):
     full_name = db.Column(db.String(100), nullable=False)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(200), nullable=False) 
+    password = db.Column(db.String(200), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False)
 
     
 
     def __repr__(self):
-        return f'<User {self.username}>'
+        return f'<User {self.username}, {self.email}, {self.full_name}>'
+
+
+class BlackListToken(db.Model):
+    __tablename__ = "blacklist_tokens"
+
+    id = db.Column(db.Integer, primary_key=True)
+    jti = db.Column(db.String(40), nullable=True, index=True)
+    created_at = db.Column(db.DateTime, default=datetime.now(timezone.utc), nullable=True)
+
