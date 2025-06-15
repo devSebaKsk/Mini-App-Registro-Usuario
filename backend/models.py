ac37297da3d42633b9b0008b628305289ed63888
Sebastian Cascante <139644093+devSebaKsk@users.noreply.github.com>
2025-06-15 11:58:57 +0000

    Model tabla user

--- backend/models.py
+++ backend/models.py
@@ -1,11 +1,15 @@
 from .app import db
 
 class User(db.Model):
-    __tablename__ = 'users' # Opcional: define explícitamente el nombre de la tabla
+    __tablename__ = 'users' 
 
     id = db.Column(db.Integer, primary_key=True)
+    full_name = db.Column(db.String(100), nullable=False)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False) 
+
+    
 
     def __repr__(self):
         return f'<User {self.username}>'
